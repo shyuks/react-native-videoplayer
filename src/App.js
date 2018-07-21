7a66e8955208ce4b59ece68fca9ba1347d078e1b
Sean Hong <s.hong35@gmail.com>
2018-07-21 02:29:37 -0700

    Create header, export to App.js and display in app

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+import { Header } from './components/common';
 
+const App = () => {
+  return (
+      <View>
+        <Header headerText="Video Player" />
+      </View>
+  )
+}
+
+export default App;
